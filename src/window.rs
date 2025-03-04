3788c2f5cb6af19110db74b842e4141f09ea9eae
Monika <>
2025-03-04 12:55:30 +0200

    Simple opengl setup

--- src/window.rs
+++ src/window.rs
@@ -1,12 +1,28 @@
+use glium::backend::glutin::glutin;
+use glium::{
+    backend::glutin::Display,
+    glutin::{
+        context::{ContextAttributes, NotCurrentGlContext, PossiblyCurrentGlContext},
+        display::GlDisplay,
+        surface::WindowSurface,
+    },
+    Surface, SwapBuffersError,
+};
+use std::{
+    ffi::{c_void, CStr, CString},
+    num::NonZero,
+};
 use winit::{
     application::ApplicationHandler,
     event::WindowEvent,
     event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    raw_window_handle::{HasRawDisplayHandle, HasRawWindowHandle},
     window::{Window as WinitWindow, WindowId},
 };
 
 pub struct Window {
     window: Option<WinitWindow>,
+    display: Option<glium::backend::glutin::Display<glutin::surface::WindowSurface>>,
 }
 
 impl ApplicationHandler for Window {
@@ -15,13 +31,62 @@ impl ApplicationHandler for Window {
             event_loop
                 .create_window(WinitWindow::default_attributes())
                 .unwrap(),
-        )
+        );
+
+        let raw_display_handle = self.window.as_ref().unwrap().raw_display_handle().unwrap();
+        let raw_window_handle = self.window.as_ref().unwrap().raw_window_handle().unwrap();
+
+        let display = Some(glutin::display::Display::Egl(
+            unsafe { glutin::api::egl::display::Display::new(raw_display_handle) }.unwrap(),
+        ));
+
+        let config = unsafe {
+            &display
+                .as_ref()
+                .unwrap()
+                .find_configs(glutin::config::ConfigTemplate::default())
+                .unwrap()
+                .nth(0)
+                .unwrap()
+        };
+
+        let surface = Some(
+            unsafe {
+                display.as_ref().unwrap().create_window_surface(
+                    config,
+                    &glutin::surface::SurfaceAttributesBuilder::<WindowSurface>::new().build(
+                        raw_window_handle,
+                        NonZero::new(100).unwrap(),
+                        NonZero::new(100).unwrap(),
+                    ),
+                )
+            }
+            .unwrap(),
+        );
+
+        let context = Some(
+            unsafe {
+                display
+                    .as_ref()
+                    .unwrap()
+                    .create_context(config, &ContextAttributes::default())
+                    .unwrap()
+                    .make_current(surface.as_ref().unwrap())
+            }
+            .unwrap(),
+        );
+
+        self.display =
+            Some(glium::backend::glutin::Display::new(context.unwrap(), surface.unwrap()).unwrap());
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => event_loop.exit(),
             WindowEvent::RedrawRequested => {
+                let mut frame = self.display.as_ref().unwrap().draw();
+                frame.clear(None, Some((1.0, 0.0, 0.0, 1.0)), true, None, None);
+                frame.finish();
                 self.window.as_ref().unwrap().request_redraw();
             }
             _ => (),
@@ -31,7 +96,10 @@ impl ApplicationHandler for Window {
 
 impl Window {
     pub fn new() -> Self {
-        Self { window: None }
+        Self {
+            window: None,
+            display: None,
+        }
     }
 
     pub fn open(&mut self) {
