ac2a256e493c81b5282dae0ff661db24f8c43e33
Monika <>
2025-03-08 09:07:00 +0200

    Added rectangle
    * Added vertex support to the renderer
    
    * Added Nalgebra as a dependency
    
    * Added a font texture image

--- src/window.rs
+++ src/window.rs
@@ -1,11 +1,14 @@
 use glium::backend::glutin::glutin;
 use glium::{
+    backend::glutin::Display,
     glutin::{
         context::{ContextAttributes, NotCurrentGlContext, PossiblyCurrentGlContext},
         display::GlDisplay,
         surface::WindowSurface,
     },
-    Surface,
+    implement_vertex,
+    index::PrimitiveType,
+    program, uniform, IndexBuffer, Program, Surface, VertexBuffer,
 };
 use std::num::NonZero;
 use winit::{
@@ -16,9 +19,46 @@ use winit::{
     window::{Window as WinitWindow, WindowId},
 };
 
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 3],
+}
+implement_vertex!(Vertex, position);
+
+struct Rectangle {
+    vao: VertexBuffer<Vertex>,
+    ebo: IndexBuffer<u16>,
+}
+
+impl Rectangle {
+    pub fn create(display: &Display<WindowSurface>) -> Self {
+        let vertex_data = [
+            Vertex {
+                position: [-1.0, -1.0, 0.0],
+            },
+            Vertex {
+                position: [-1.0, 1.0, 0.0],
+            },
+            Vertex {
+                position: [1.0, -1.0, 0.0],
+            },
+            Vertex {
+                position: [1.0, 1.0, 0.0],
+            },
+        ];
+        let index_data = [0, 1, 2, 3, 2, 1];
+        Self {
+            vao: VertexBuffer::new(display, &vertex_data).unwrap(),
+            ebo: IndexBuffer::new(display, PrimitiveType::TrianglesList, &index_data).unwrap(),
+        }
+    }
+}
+
 pub struct Window {
     window: Option<WinitWindow>,
-    display: Option<glium::backend::glutin::Display<glutin::surface::WindowSurface>>,
+    display: Option<Display<WindowSurface>>,
+    rect: Option<Rectangle>,
+    program: Option<Program>,
 }
 
 impl ApplicationHandler for Window {
@@ -74,14 +114,47 @@ impl ApplicationHandler for Window {
 
         self.display =
             Some(glium::backend::glutin::Display::new(context.unwrap(), surface.unwrap()).unwrap());
+
+        self.rect = Some(Rectangle::create(self.display.as_ref().unwrap()));
+
+        self.program = Some(
+            program!(self.display.as_ref().unwrap(),
+            330 => {
+                vertex: r#"
+                    #version 330 core
+                    layout (location=0) in vec3 position;
+                    void main() {
+                        gl_Position = vec4(position, 1.0);
+                    }
+                "#,
+                fragment: r#"
+                    #version 330 core
+                    out vec4 color;
+                    void main() {
+                        color = vec4(1.0, 0.0, 0.0, 1.0);
+                    }
+                "#
+            })
+            .unwrap(),
+        );
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => event_loop.exit(),
             WindowEvent::RedrawRequested => {
+                let uniforms = uniform![];
                 let mut frame = self.display.as_ref().unwrap().draw();
-                frame.clear(None, Some((1.0, 0.0, 0.0, 1.0)), true, None, None);
+                frame.clear(None, Some((0.2, 0.2, 0.2, 1.0)), true, None, None);
+                frame
+                    .draw(
+                        &self.rect.as_ref().unwrap().vao,
+                        &self.rect.as_ref().unwrap().ebo,
+                        self.program.as_ref().unwrap(),
+                        &uniforms,
+                        &Default::default(),
+                    )
+                    .unwrap();
                 frame.finish().expect("Failed to finish frame draw");
                 self.window.as_ref().unwrap().request_redraw();
             }
@@ -95,6 +168,8 @@ impl Window {
         Self {
             window: None,
             display: None,
+            rect: None,
+            program: None,
         }
     }
 
